184dfd320617a26f960540848203224879708b85
iel0000 <magsinoariel@gmail.com>
2022-09-12 23:05:50 +0800

    update environment and add htaccess

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:44312',
+  apiUrl: 'http://localhost:44312',
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    environmentName: 'Stage',
+    apiUrl: 'http://stage-webapi.nationwidehealthsystems.com'
+  };
